c52834d710c6349b7d103012fb533d78ceca0e93
Alejandro <alejandro.cabanas-rojas@amido.com>
2015-11-02 16:45:40 +0000

    updated lambda to v3

--- HelloHendrix.js
+++ HelloHendrix.js
@@ -1,7 +1,7 @@
-console.log('Loading new version');
+console.log('Loading new version 3');
 
 exports.handler = function(event, context) {
-    console.log('Hello Hendrix! v2');
+    console.log('Hello Hendrix! v3');
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
