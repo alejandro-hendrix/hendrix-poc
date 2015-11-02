119fefc70639d1bb0396a6913a08f7eb0a0ebfa9
Alejandro <alejandro.cabanas-rojas@amido.com>
2015-11-02 16:48:49 +0000

    version 4

--- HelloHendrix.js
+++ HelloHendrix.js
@@ -1,7 +1,7 @@
-console.log('Loading new version 3');
+console.log('Loading new version 4');
 
 exports.handler = function(event, context) {
-    console.log('Hello Hendrix! v3');
+    console.log('Hello Hendrix! v4');
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
