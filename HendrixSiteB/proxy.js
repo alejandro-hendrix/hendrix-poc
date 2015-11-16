b65469a2675c4867b5d1cffdf18a5581be40b665
Alejandro <alejandro.cabanas-rojas@amido.com>
2015-11-16 17:17:22 +0000

    posting to authorize with valid access token

--- HendrixSiteB/proxy.js
+++ HendrixSiteB/proxy.js
@@ -1,6 +1,18 @@
 $(function(){
 			if (location.hash){
-				alert(location.hash);				
+				var lochash    = location.hash.substr(1),
+    			 accessToken = lochash.substr(lochash.indexOf('access_token='))
+                  .split('&')[0]
+                  .split('=')[1];		
+                  $.post('https://amidoltd.auth0.com/oauth/access_token',
+					{
+					  "client_id":    "nXwKxdF38kIXWV8dG11vFxEOOCw6BwjM", 
+					  "access_token": accessToken,
+					  "connection":   "facebookTest2",
+					  "scope": "openid profile"					  
+					}, function(data){
+						alert(data);
+					});		
 			}
 
 			$('#login').click(function(){
